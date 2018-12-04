56499296da1441dc36ac40beaeed52d14d96a982
andresglzca <andresglzca@gmail.com>
2018-12-04 04:28:02 -0600

    cambios de diseño

--- src/pages/temp/temp.ts
+++ src/pages/temp/temp.ts
@@ -15,21 +15,29 @@ export class TempPage {
   tempRef2: Observable<any[]>;;
   tempRef3: Observable<any[]>;;
 
-  lastTemp: any;
-  lastTemp2: any;
-  lastTemp3: any;
+  lastTemp: any = '--';
+  lastTemp2: any = '--';
+  lastTemp3: any = '--';
 
-  cardColor1;
-  cardColor2;
-  cardColor3;
+  cardColor1: string = "tempInit";
+  cardColor2: string = "tempInit";
+  cardColor3: string = "tempInit";
   cardColorProm;
 
   promedio: number;
+  assetTemp: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public angularDB: AngularFireDatabase, private localNot: LocalNotifications) {
 
-    this.tempRef = angularDB.list('sensor', ref => ref.limitToLast(1)).valueChanges();
+
+
+  }
+
+  initData() {
+    this.tempRef = this.angularDB.list('sensor', ref => ref.limitToLast(1)).valueChanges();
     this.tempRef.subscribe(dato => {
+      console.log(dato);
+      
       dato.map(temp => {
         console.log('S1: ' + temp.temp);
         if (temp.temp <= 9) {
@@ -58,7 +66,7 @@ export class TempPage {
       });
     });
 
-    this.tempRef2 = angularDB.list('sensor2', ref => ref.limitToLast(1)).valueChanges();
+    this.tempRef2 = this.angularDB.list('sensor2', ref => ref.limitToLast(1)).valueChanges();
     this.tempRef2.subscribe(dato => {
       dato.map(temp => {
         console.log('S2: ' + temp.temp);
@@ -90,7 +98,7 @@ export class TempPage {
       });
     });
 
-    this.tempRef3 = angularDB.list('sensor3', ref => ref.limitToLast(1)).valueChanges();
+    this.tempRef3 = this.angularDB.list('sensor3', ref => ref.limitToLast(1)).valueChanges();
     this.tempRef3.subscribe(dato => {
       dato.map(temp => {
         console.log('S3: ' + temp.temp);
@@ -121,7 +129,6 @@ export class TempPage {
       });
     });
 
- 
 
   }
 
@@ -130,17 +137,25 @@ export class TempPage {
 
     if (this.promedio <= 9) {
       this.cardColorProm = "tempPromDown";
+      this.assetTemp = "tempDown.png";
     }
     if (this.promedio >= 10) {
       this.cardColorProm = "tempPromOk";
+      this.assetTemp = "tempOk.png";
     }
     if (this.promedio >= 27) {
       this.cardColorProm = "tempPromWarning"
+      this.assetTemp = "tempWarning.png";
     }
     if (this.promedio >= 30) {
       this.cardColorProm = "tempPromUp";
+      this.assetTemp = "tempUp.png";
     }
   }
- 
+
+  ionViewDidLoad() {
+   this.initData();
+  }
+
 
 }
