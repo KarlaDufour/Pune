636ef5f1423af721fad502b18e040c38d411df05
Karla_Dufour <mimeduak@outlook.com>
2019-03-14 22:20:42 -0600

    '?'

--- src/pages/charts/charts.ts
+++ src/pages/charts/charts.ts
@@ -3,15 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs-compat';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Chart } from 'chart.js';
-
-
-/**
- * Generated class for the ChartsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+ 
 @IonicPage()
 @Component({
   selector: 'page-charts',
