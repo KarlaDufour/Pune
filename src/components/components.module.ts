9fc6925988e79db19943958c1529923df51145ee
Karla_Dufour <mimeduak@outlook.com>
2019-03-14 22:25:08 -0600

    '?'

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { NotsComponent } from './nots/nots';
+@NgModule({
+	declarations: [NotsComponent],
+	imports: [],
+	exports: [NotsComponent]
+})
+export class ComponentsModule {}
